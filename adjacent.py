c6004cbd4d58bff536eb013c4ecf4286223eb04e
Evgeniy Sorokin <tweakon@gmail.com>
2020-02-18 14:18:39 +0300

    adjacent.py refactoring

--- adjacent.py
+++ adjacent.py
@@ -4,22 +4,26 @@ class Adjacent:
         self.adj = []
         self.w = len(grid[0])
         self.h = len(grid)
+        self.grid = grid
         for r, row in enumerate(grid):
             for c, col in enumerate(row):
                 adjacent = []
-                if r-1 >= 0 and grid[r-1][c] == 0:
-                    adjacent.append(c+len(row)*(r-1))
-                if r+1 < len(grid) and grid[r+1][c] == 0:
-                    adjacent.append(c+len(row)*(r+1))
-                if c-1 >= 0 and grid[r][c-1] == 0:
-                    adjacent.append(c-1+len(row)*r)
-                if c+1 < len(row) and grid[r][c+1] == 0:
-                    adjacent.append(c+1+len(row)*r)
+                self.check_add_adj(r, c-1, adjacent)
+                self.check_add_adj(r, c+1, adjacent)
+                self.check_add_adj(r-1, c, adjacent)
+                self.check_add_adj(r+1, c, adjacent)
                 self.adj.append(adjacent)
+
+    def check_add_adj(self, r, c, adj):
+        if 0 <= r < self.h and 0 <= c < self.w and self.grid[r][c] == 0:
+            adj.append(self.to_seq(r, c))
+
+    def to_seq(self, r, c):
+        return c + self.h*r
 
     def get(self, r, c=None):
         n = r
         if c is not None:
-            n = c + self.w*r
+            n = self.to_seq(r, c)
         return self.adj[n]
 
